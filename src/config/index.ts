ada3f508b2dd3077d11fa2ae8ef0d53ae58bccb8
Stanislav Polianychko <staspolianychko@gmail.com>
2024-06-08 10:11:35 +0200

    feat[config]: configured, loading added

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,17 +1,23 @@
 abstract class AppConfig {
-    public static apiUrl: string = '';
-    public static apiKey: string = '';
+    public static personBaseUrl: string | undefined;
+    public static imageBaseUrl: string | undefined;
 
     public static load(): void {
-        const apiUrl: string | undefined = process.env.NEXT_PUBLIC_API_URL;
-        if (apiUrl) {
-            this.apiUrl = apiUrl;
+        const personBaseUrl: string | undefined =
+            process.env.NEXT_PUBLIC_PERSON_BASE_API_URL;
+        if (!personBaseUrl) {
+            console.error('Person base API URL is not set');
+            process.exit(1);
         }
+        this.personBaseUrl = apiUrl;
 
-        const apiKey: string | undefined = process.env.REACT_APP_API_KEY;
-        if (apiKey) {
-            this.apiKey = apiKey;
+        const imageBaseUrl: string | undefined =
+            process.env.NEXT_PUBLIC_IMAGE_BASE_API_URL;
+        if (!imageBaseUrl) {
+            console.error('Image base API URL is not set');
+            process.exit(1);
         }
+        this.imageBaseUrl = apiKey;
     }
 }
 
